b4c485de5643a1a283c011e57ffef3be70b7160d
tianshengjie <tianshengjie@howzhi.com>
2017-11-22 12:55:20 +0800

    add alert componet

--- src/AppBundle/Resources/static-src/js/article/create/article.js
+++ src/AppBundle/Resources/static-src/js/article/create/article.js
@@ -9,6 +9,8 @@ class Article {
         $.post($form.attr('action'), $form.serialize(), function(message){
             if (message.error) {
                 $.alert.show(message.error, 'danger', 3000);
+            } else {
+                $.alert.show('保存成功', 'success', 3000);
             }
             $form.find('#article-id').val(message.id);
         });
--- src/AppBundle/Resources/static-src/js/libs/alert.js
+++ src/AppBundle/Resources/static-src/js/libs/alert.js
@@ -1,12 +1,12 @@
 class Alert {
     constructor(setting){
         this.body = $('body');
-        this.temp =  $(`<div id="alert" class="alert text-center fixed transition" role="alert"></div>`);
+        this.temp =  $(`<div id="alert" class="alert text-center fixed transition" role="alert"><div id="alert-content"></div></div>`);
         this.body.prepend(this.temp);
     }
 
     show(message, level, time = 1000) {
-        this.temp.text(message).addClass('show alert-' + level);
+        this.temp.addClass('show').find('#alert-content').text(message).removeClass().addClass('alert-' + level);
         this.hide(time);
     }
 
