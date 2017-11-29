b7ea6e5566268e35e8e951b69565b9a7c0421b3a
tianshengjie <tianshengjie@howzhi.com>
2017-11-29 17:45:32 +0800

    首页显示简介，引入字体文件

--- src/AppBundle/Twig/WebExtension.php
+++ src/AppBundle/Twig/WebExtension.php
@@ -3,4 +3,21 @@ namespace AppBundle\Twig;
 
 class WebExtension extends \Twig_Extension
 {
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('filter_html', array($this, 'filterHtml')),
+            new \Twig_SimpleFilter('substr', array($this, 'substr')),
+        );
+    }
+
+    public function filterHtml($value)
+    {
+        return strip_tags($value);
+    }
+
+    public function substr($value, $length)
+    {
+        return mb_substr($value, 0, $length) . '...';
+    }  
 }
